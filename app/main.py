2331f371b41d11e5b8769e6ba2001b95e1928716
keleseth <shasa.keles@gmail.com>
2025-08-19 14:15:02 +0300

    dockerfiles updated

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import os
 from redis.asyncio import Redis
 
-from .config import settings
+from app.config import settings
 
 app = FastAPI(
     title=settings.app_title,
